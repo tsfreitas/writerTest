10c84301af0cbe46a384cd08fce4d32cc75288a1
THIAGO SANTOS DE FREITAS <tsfreitas@gmail.com>
2016-06-19 15:13:50 -0300

    Guava charsinc

--- src/main/java/com/tsfreitas/writeTest/writer/GuavaWriter.java
+++ src/main/java/com/tsfreitas/writeTest/writer/GuavaWriter.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
 
+import com.google.common.io.CharSink;
+import com.google.common.io.FileWriteMode;
 import org.springframework.stereotype.Component;
 
 import com.google.common.base.Charsets;
@@ -13,16 +15,17 @@ import com.google.common.io.Files;
 @Component
 public class GuavaWriter implements Writer {
 
-	@Override
-	public void writeFile(String fileName, List<String> lines) throws IOException {
+    @Override
+    public void writeFile(String fileName, List<String> lines) throws IOException {
 
-		Path path = Writer.createPath(fileName);
-		File file = path.toFile();
+        Path path = Writer.createPath(fileName);
+        File file = path.toFile();
 
-		for (String line : lines) {
-			Files.append(line, file, Charsets.UTF_8);
-		}
+        CharSink sink = Files.asCharSink(file, Charsets.UTF_8, FileWriteMode.APPEND);
 
-	}
+        sink.writeLines(lines);
 
+
+
+    }
 }
